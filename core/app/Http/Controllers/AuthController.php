cf959264ead4cd01539a554670fccc658d58eddf
Alihan Öztürk <alihanozturk364@gmail.com>
2024-11-26 22:21:21 +0300

    project started

---
+++ core/app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response()->json(Auth::user(), 200);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function user()
+    {
+        return response()->json(Auth::user());
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json(['message' => 'Logged out']);
+    }
+}
+
